08eff12bc5e11e46d6272fd378a948f3135342bb
Ika <rexar.ika@gmail.com>
2025-09-12 00:13:45 +0300

    hikari, hibernate with empty bd, DB OK

--- src/main/java/web/AppBootstrapListener.java
+++ src/main/java/web/AppBootstrapListener.java
@@ -4,6 +4,9 @@ import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.WebApplicationTemplateResolver;
@@ -11,6 +14,9 @@ import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 
 @WebListener
 public class AppBootstrapListener implements ServletContextListener {
+
+    private SessionFactory sessionFactory;
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext servletContext = sce.getServletContext();
@@ -27,10 +33,31 @@ public class AppBootstrapListener implements ServletContextListener {
 
         servletContext.setAttribute("templateEngine", engine);
         servletContext.setAttribute("jakartaApp", application);
+        try {
+            Configuration cfg = new Configuration();
+
+            // cfg.addAnnotatedClass(User.class);
+            //SessionFactory sf = (SessionFactory) getServletContext().getAttribute("sessionFactory"); это для сервлета
+
+            sessionFactory = cfg.buildSessionFactory();
+            servletContext.setAttribute("sessionFactory", sessionFactory);
+
+            System.out.println("Hibernate SessionFactory initialized");
+
+            try (Session session = sessionFactory.openSession()) {
+                session.createNativeQuery("SELECT 1").getSingleResult();
+                System.out.println("DB OK");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка инициализации Hibernate", e);
+        }
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-
+        if (sessionFactory != null) {
+            sessionFactory.close();
+            System.out.println("Hibernate SessionFactory closed");
+        }
     }
 }
